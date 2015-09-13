07bcd50cf0af26582a98bf1a1cb0a1601afa3e3b
mpgreg <milesper@gmail.com>
2015-09-13 21:04:07 +0200

    Initial commit

---
+++ plot1.R
@@ -0,0 +1,73 @@
+## CDSS Exploratory Data Analysis
+## Project 1 - Plot 1
+
+## Written by: Michael Gregory
+## Date: 13-Sep-2015
+
+## Set some general variables
+## Original data description at http://archive.ics.uci.edu/ml/datasets/Individual+household+electric+power+consumption
+##  and http://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip
+
+## Based on fork/clone from https://github.com/rdpeng/ExData_Plotting1
+
+
+fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+trueFileMD5 <- "41f51806846b6b567b8ae701a300a3de"
+
+##setwd("~/Documents/School/coursera/data science/exploratory data analysis/project1")
+baseDir <- getwd()
+
+zipFile <- paste(baseDir,"/household_power_consumption.zip", sep="")
+dataFile <- paste(baseDir,"/household_power_consumption.txt", sep="")
+dateDownloaded <- NULL
+
+outputFile <- paste(baseDir,"/plot1.png", sep="")
+
+##If the file doesn't already exist in baseDir download it and check md5
+if(!file.exists(zipFile)) {
+        cat(sprintf("Downloading file %s \n\t to location %s.\n", fileURL, zipFile))
+        download.file(fileURL, destfile=zipFile, method="curl",quiet=TRUE)
+        dateDownloaded <- date()
+} else cat(sprintf("Zipped data file already exists in specified location. \n\t Using File: %s \n",zipFile))
+
+zipFileMD5 <- digest::digest(algo = "md5", file=zipFile)
+if(zipFileMD5 != trueFileMD5) {
+        warning("Downloaded file has changed from original source for this script.  
+                        This script may not work as originally intended.")
+}
+
+## Load the data from the dates 2007-02-01 and 2007-02-02. 
+cat(sprintf("Reading in dataset from file: \n\t%s\n", dataFile))
+powerConsDF <- read.table(pipe('grep "^[1-2]/2/2007\\|^Date" "household_power_consumption.txt"'), 
+                          sep=";",na.strings="?",header=TRUE)
+
+##Would like to do it from the original zip file without unzipping
+##zipFileCon<-unz(zipFile,dataFile)
+##open(zipFileCon)
+##powerConsDF <- read.table(pipe('grep "^[1-2]/2/2007\\|^Date" zipFileCon)'), 
+##                          sep=";",na.strings="?",header=TRUE)
+##close(zipFileCon)
+
+##  Convert the Date and Time variables to Date/Time classes
+powerConsDF$Date <- as.Date(powerConsDF$Date,format='%d/%m/%Y')
+powerConsDF$DateTime <- paste(powerConsDF$Date, powerConsDF$Time)
+powerConsDF$DateTime<-strptime(powerConsDF$DateTime, format = "%Y-%m-%d %H:%M:%S",tz="UTC")
+
+##Making Plots
+##  Examine how household energy usage varies over a 2-day period in February, 2007. 
+
+##Open png file 
+cat(sprintf("Opening output file: \n\t%s\n", outputFile))
+png(filename = outputFile)
+
+##Create the image
+hist(powerConsDF$Global_active_power,main = "Global Active Power",
+     col="red",
+     xlab="Global Active Power (kilowatts)",
+     ylab="Frequency")
+
+##Save/close the image
+dev.off()
+
+
+
---
+++ plot4.R
@@ -0,0 +1,95 @@
+## CDSS Exploratory Data Analysis
+## Project 1 - Plot 4
+
+## Written by: Michael Gregory
+## Date: 13-Sep-2015
+
+## Set some general variables
+## Original data description at http://archive.ics.uci.edu/ml/datasets/Individual+household+electric+power+consumption
+##  and http://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip
+
+## Based on fork/clone from https://github.com/rdpeng/ExData_Plotting1
+
+
+fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+trueFileMD5 <- "41f51806846b6b567b8ae701a300a3de"
+
+##setwd("~/Documents/School/coursera/data science/exploratory data analysis/project1")
+baseDir <- getwd()
+
+zipFile <- paste(baseDir,"/household_power_consumption.zip", sep="")
+dataFile <- paste(baseDir,"/household_power_consumption.txt", sep="")
+dateDownloaded <- NULL
+
+outputFile <- paste(baseDir,"/plot4.png", sep="")
+
+##If the file doesn't already exist in baseDir download it and check md5
+if(!file.exists(zipFile)) {
+        cat(sprintf("Downloading file %s \n\t to location %s.\n", fileURL, zipFile))
+        download.file(fileURL, destfile=zipFile, method="curl",quiet=TRUE)
+        dateDownloaded <- date()
+} else cat(sprintf("Zipped data file already exists in specified location. \n\t Using File: %s \n",zipFile))
+
+zipFileMD5 <- digest::digest(algo = "md5", file=zipFile)
+if(zipFileMD5 != trueFileMD5) {
+        warning("Downloaded file has changed from original source for this script.  
+                This script may not work as originally intended.")
+}
+
+## Load the data from the dates 2007-02-01 and 2007-02-02. 
+cat(sprintf("Reading in dataset from file: \n\t%s\n", dataFile))
+powerConsDF <- read.table(pipe('grep "^[1-2]/2/2007\\|^Date" "household_power_consumption.txt"'), 
+                          sep=";",na.strings="?",header=TRUE)
+
+##  Convert the Date and Time variables to Date/Time classes
+powerConsDF$Date <- as.Date(powerConsDF$Date,format='%d/%m/%Y')
+powerConsDF$DateTime <- paste(powerConsDF$Date, powerConsDF$Time)
+powerConsDF$DateTime<-strptime(powerConsDF$DateTime, format = "%Y-%m-%d %H:%M:%S",tz="UTC")
+
+##Making Plots
+##  Examine how household energy usage varies over a 2-day period in February, 2007. 
+
+##Open png file 
+cat(sprintf("Opening output file: \n\t%s\n", outputFile))
+png(filename = outputFile)
+
+##Create the image framing 2x2
+par(mfrow = c(2,2))
+
+##Insert top/left
+plot(powerConsDF$DateTime, powerConsDF$Global_active_power, 
+     ylab = "Global Active Power",
+     xlab = "",
+     type="l")
+
+##Insert top/right
+plot(powerConsDF$DateTime, powerConsDF$Voltage, 
+     ylab = "Voltage",
+     xlab = "datetime",
+     type="l")
+
+##Insert bottom/left
+plot(powerConsDF$DateTime, powerConsDF$Sub_metering_1, 
+     ylab = "Engergy sub metering", 
+     xlab = "", 
+     type="l", 
+     col = "black")
+lines(powerConsDF$DateTime, powerConsDF$Sub_metering_2, 
+      type="l", 
+      col = "red")
+lines(powerConsDF$DateTime, powerConsDF$Sub_metering_3, 
+      type="l", 
+      col = "blue")
+legend("topright", lty=1, 
+       col = c("black", "red", "blue"), 
+       legend = names(powerConsDF[7:9]),
+       bty = "n")
+
+##Insert bottom/right
+plot(powerConsDF$DateTime, powerConsDF$Global_reactive_power, 
+     ylab = "Global_reactive_power",
+     xlab = "datetime",
+     type="l")
+
+##Save/close the image
+dev.off()
