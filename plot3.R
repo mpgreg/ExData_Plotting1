2249c109161dc391e5f2ef5099ec79d44c49f404
mpgreg <milesper@gmail.com>
2015-09-14 00:36:14 +0200

    changed filter to platform-independent sql

--- plot3.R
+++ plot3.R
@@ -10,6 +10,7 @@
 
 ## Based on fork/clone from https://github.com/rdpeng/ExData_Plotting1
 
+library(sqldf)
 
 fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 trueFileMD5 <- "41f51806846b6b567b8ae701a300a3de"
@@ -30,19 +31,27 @@ if(!file.exists(zipFile)) {
         dateDownloaded <- date()
 } else cat(sprintf("Zipped data file already exists in specified location. \n\t Using File: %s \n",zipFile))
 
+
 zipFileMD5 <- digest::digest(algo = "md5", file=zipFile)
 if(zipFileMD5 != trueFileMD5) {
         warning("Downloaded file has changed from original source for this script.  
-                This script may not work as originally intended.")
+                        This script may not work as originally intended.")
 }
+
+if(!file.exists(dataFile)) {
+        cat(sprintf("Unzipping file %s \n\t ", dataFile))
+        unzip(zipFile, files=dataFile)
+} else cat(sprintf("Unzipped data file already exists. \n\t Using data file: %s \n",dataFile))
+
 
 ## Load the data from the dates 2007-02-01 and 2007-02-02. 
 cat(sprintf("Reading in dataset from file: \n\t%s\n", dataFile))
-unzip(zipFile, files=dataFile)
-powerConsDF <- read.table(pipe('grep "^[1-2]/2/2007\\|^Date" "household_power_consumption.txt"'), 
-                          sep=";",
-                          na.strings="?",
-                          header=TRUE)
+zipFileCon <- file(dataFile)
+powerConsDF<-sqldf("select * from zipFileCon where Date='1/2/2007' or Date='2/2/2007'",file.format = list(header=TRUE,sep=";"))
+close(zipFileCon)
+
+##Alternative non-portable option
+##powerConsDF <- read.table(pipe('grep "^[1-2]/2/2007\\|^Date" "household_power_consumption.txt"'), sep=";", na.strings="?", header=TRUE)
 
 ##  Convert the Date and Time variables to Date/Time classes
 powerConsDF$Date <- as.Date(powerConsDF$Date,format='%d/%m/%Y')
